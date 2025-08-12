9129920f2aa006226e4ce862b03479fb5fa56ce8
unnathianalytics <unnathianalytics@gmail.com>
2025-08-12 21:34:10 +0530

    Custom Theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,6 +25,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->id('admin')
             ->path('admin')
             ->login()
